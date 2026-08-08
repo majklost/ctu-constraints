86085159224a90dd24c176620a5b0b1d5b92a6b2
majklost <majkl.mrkos@seznam.cz>
2026-08-08 19:03:54 +0200

    report, before rewrite discussion

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from constraints.test import f
 from pathlib import Path
+
 print(Path(__file__).name)
+
+# %%
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+# %%
+t = np.random.random((200, 200))
+
+# %%
+plt.imshow(t)
+# %%
+p = pd.DataFrame({"prd": [1, 2, 3, 4]})
+p.head()
+# %%
+import torch
+
+torch.cuda.is_available()
+
+# %%
